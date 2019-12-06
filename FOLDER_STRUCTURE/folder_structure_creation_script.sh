e3a495994c378385979da293f28947a9b7f4009e
Sohom Ghosh <sohom1ghosh@gmail.com>
2019-12-06 23:11:32 +0530

    Create folder_structure_creation_script.sh

---
+++ FOLDER_STRUCTURE/folder_structure_creation_script.sh
@@ -0,0 +1,10 @@
+mkdir 0_Notes
+mkdir 1_data
+mkdir 2_EDA
+mkdir 3_data_preparation_feature_engg_notebooks
+mkdir 4_modelling_notebooks
+mkdir 5_final_scripts
+mkdir 6_submission_files
+mkdir utils
+mv *.csv 1_data/
+touch 0_Notes/notes.txt
